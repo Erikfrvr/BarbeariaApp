8d7afc55f19e05f8f7fae57489ac48c6e03a9b8e
DaiHoss <duartedaiane458@gmail.com>
2026-05-09 18:57:48 -0300

    update agendamento_dao
    alterações feitas

--- BarbeariaApp/src/model/DAO/agendamento_dao.py
+++ BarbeariaApp/src/model/DAO/agendamento_dao.py
@@ -2,13 +2,13 @@ from src.model.base_db import BaseDB
 
 class Agendamento_DAO:
     def __init__(self):
-        #conecta com o arquivo agendamento.json
+        # conecta com o arquivo agendamento.json
         self.db=BaseDB("agendamento.json")
 
     def addAgendamento(self,data:dict):
-        agendamentos=self.db.read()
+        agendamentos = self.db.read()
         agendamentos.append(data)
-        self.db.weite(agendamentos)
+        self.db.write(agendamentos)
 
     def lerAgendamentos(self):
         return self.db.read()
@@ -16,11 +16,11 @@ class Agendamento_DAO:
     def deletarAgendamento(self,id_agendamento:int):
         agendamentos=self.db.read()
         nova_lista=[a for a in agendamentos if a["id"] !=id_agendamento]
-        self.db.weite(nova_lista)
+        self.db.write(nova_lista)
 
     def buscarPorID(self,id:int):
         agendamentos=self.db.read()
         for a in agendamentos:
-            if a ["id"]==id:
+            if a["id"]==id:
                 return a
         return None
