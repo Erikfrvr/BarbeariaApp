51e6e6735c2364d745e5b39f289f1c2c070fbbb0
Erik Mauricio <erikmauricio3@hotmail.com>
2026-05-12 09:35:28 -0300

    Update base_db.py
    classe BaseDB

--- BarbeariaApp/src/model/DAO/base_db.py
+++ BarbeariaApp/src/model/DAO/base_db.py
@@ -1,1 +1,12 @@
-a=1
+import json
+from json import JSONDecodeError
+import os
+
+
+class BaseDB:
+    def __init__(self, file_db:str):
+        # Define o caminho absoluto do banco para evitar erros de path
+        diretorio_atual=os.path.dirname(os.path.abspath(__file__))
+        self.__path=os.path.join(diretorio_atual, file_db)
+        self.__file_db=file_db
+
